8c4c1e6f360db41117c7965063430b885e335c6d
akman12914 <rnlduadns98@naver.com>
2024-10-24 13:47:19 +0900

    주석 제거 (빌드 오류 수정됨)

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./**/*.html"],
-  // html 파일 안에서 tailwind를 사용함
-  // 오류 "./src/**/*.html" < src 안의 html에만 적용됨
   theme: {
     extend: {},
   },
